0ec94b332dd63ec76120f987852a2e79d51d667a
Elattari Mohssine <43855033+MohssineElAttari@users.noreply.github.com>
2022-02-25 13:35:55 +0100

    first commit

---
+++ src/main/java/com/example/docker/springbootbostgresdocker/SpringbootBostgresDockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.docker.springbootbostgresdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBostgresDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBostgresDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/docker/springbootbostgresdocker/SpringbootBostgresDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.docker.springbootbostgresdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBostgresDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
