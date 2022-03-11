4b3c55ed0495aae33506460817a7922cecfd00af
Elattari Mohssine <43855033+MohssineElAttari@users.noreply.github.com>
2022-03-12 00:55:31 +0100

    new update

--- src/main/java/com/example/docker/springbootbostgresdocker/SpringbootBostgresDockerApplication.java
+++ src/main/java/com/example/docker/springbootbostgresdocker/SpringbootBostgresDockerApplication.java
@@ -2,10 +2,11 @@ package com.example.docker.springbootbostgresdocker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpringbootBostgresDockerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBostgresDockerApplication.class, args);
 	}
